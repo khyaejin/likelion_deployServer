a55e1bce7b2cbb27f9a5ea34c8aa924c444df47e
junhaa <2171326@hansung.ac.kr>
2024-03-30 18:04:27 +0900

    Section 5-2 : 회원 웹 기능 - 등록

--- junha/hw1/src/main/java/hello/hellospring/web/controller/HomeController.java
+++ junha/hw1/src/main/java/hello/hellospring/web/controller/HomeController.java
@@ -1,4 +1,4 @@
-package hello.hellospring.controller;
+package hello.hellospring.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ junha/hw1/src/main/java/hello/hellospring/web/controller/MemberController.java
@@ -0,0 +1,33 @@
+package hello.hellospring.web.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import hello.hellospring.web.dto.MemberForm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+	private final MemberService memberService;
+
+	@Autowired
+	public MemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+
+	@GetMapping(value = "/members/new")
+	public String createForm() {
+		return "members/createMemberForm";
+	}
+
+	@PostMapping(value = "/members/new")
+	public String create(MemberForm form) {
+		Member member = new Member();
+		member.setName(form.getName());
+		memberService.join(member);
+		return "redirect:/";
+	}
+}
