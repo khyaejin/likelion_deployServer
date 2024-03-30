a2e1ce92f876e565bbef799fa453cbf466b59ead
junhaa <2171326@hansung.ac.kr>
2024-03-30 18:12:54 +0900

    Section 6-2 : 순수 JDBC

--- junha/hw1/src/main/java/hello/hellospring/config/SpringConfig.java
+++ junha/hw1/src/main/java/hello/hellospring/config/SpringConfig.java
@@ -1,4 +1,7 @@
 package hello.hellospring.config;
+import javax.sql.DataSource;
+
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -6,12 +9,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
+	private final DataSource dataSource;
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
 	}
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		// return new MemoryMemberRepository();
+		// h2로 변경
+		return new JdbcMemberRepository(dataSource);
 	}
 }
