6ef42f84322e473104c6da86b38f5dd4c948c894
junhaa <2171326@hansung.ac.kr>
2024-03-30 18:32:33 +0900

    Section 6-6 : 스프링 데이터 JPA

--- junha/hw1/src/main/java/hello/hellospring/config/SpringConfig.java
+++ junha/hw1/src/main/java/hello/hellospring/config/SpringConfig.java
@@ -1,36 +1,22 @@
 package hello.hellospring.config;
-import javax.sql.DataSource;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class SpringConfig {
-	private final DataSource dataSource;
-	private final EntityManager em;
-	public SpringConfig(DataSource dataSource, EntityManager em) {
-		this.em = em;
-		this.dataSource = dataSource;
+	private final MemberRepository memberRepository;
+
+	public SpringConfig(@Qualifier("springDataJpaMemberRepository") MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
 	}
+
 	@Bean
 	public MemberService memberService() {
-		return new MemberService(memberRepository());
-	}
-	@Bean
-	public MemberRepository memberRepository() {
-		// return new MemoryMemberRepository();
-
-		// return new JdbcMemberRepository(dataSource);
-
-		// return new JdbcTemplateMemberRepository(dataSource);
-
-		return new JpaMemberRepository(em);
+		return new MemberService(memberRepository);
 	}
 }
