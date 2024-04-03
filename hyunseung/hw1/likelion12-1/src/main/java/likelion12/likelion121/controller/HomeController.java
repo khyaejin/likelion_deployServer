e84ef05b7ddb34354d3aab0ba14391153ff4702a
이현승 <77336664+gesal03@users.noreply.github.com>
2024-04-03 16:51:32 +0900

    Section 5-1 : 회원 웹 기능 - 홈 화면 추가

---
+++ hyunseung/hw1/likelion12-1/src/main/java/likelion12/likelion121/controller/HomeController.java
@@ -0,0 +1,12 @@
+package likelion12.likelion121.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
