9396205df0b10887837d58f48223d82c98606bba
이현승 <77336664+gesal03@users.noreply.github.com>
2024-04-03 11:00:53 +0900

    Section 1: 프로젝트 환경 설정

---
+++ hyunseung/hw1/likelion12-1/src/main/java/likelion12/likelion121/Likelion121Application.java
@@ -0,0 +1,13 @@
+package likelion12.likelion121;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Likelion121Application.class, args);
+	}
+
+}
