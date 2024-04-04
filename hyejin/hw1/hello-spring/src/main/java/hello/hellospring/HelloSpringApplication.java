67cf7e59ef51c76708354508cda2f507a6f90240
khyaejin <olivia019182@naver.com>
2024-04-04 10:06:02 +0900

    Section 1-1 : 프로젝트 생성

--- hyejin/hw1/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hyejin/hw1/hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //run 후 http://localhost:8080/HelloJSP2/hello2.jsp으로 웹 페이지 접근
 		SpringApplication.run(HelloSpringApplication.class, args);
-	}
+		}
 
 }
