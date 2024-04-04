6d43435c4db21aaf5ab684a735b95d4674c3bcb5
이현승 <77336664+gesal03@users.noreply.github.com>
2024-04-04 23:05:57 +0900

    Section 6-6 : Spring Data JPA

--- hyunseung/hw1/likelion12-1/src/main/java/likelion12/likelion121/service/SpringConfig.java
+++ hyunseung/hw1/likelion12-1/src/main/java/likelion12/likelion121/service/SpringConfig.java
@@ -2,6 +2,7 @@ package likelion12.likelion121.service;
 
 import jakarta.persistence.EntityManager;
 import likelion12.likelion121.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,31 +12,36 @@ import javax.sql.DataSource;
 public class SpringConfig {
 
     //Jdbc
-    private final DataSource dataSource;
-    // JPA
-    private final EntityManager em;
+//    private final DataSource dataSource;
+//    // JPA
+//    private final EntityManager em;
+//
+//    public SpringConfig(DataSource dataSource, EntityManager em) {
+//        this.dataSource = dataSource;
+//        this.em = em;
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-//        return new MemoryMemberRepository();
 
-        // 순수 Jdbc
-//        return new JdbcMemberRepository(dataSource);
-
-        // Jdbc Template
-//        return new JdbcTemplateMemberRepository(dataSource);
-
-        //JPA
-        return new JpaMemberRepository(em);
-
-    }
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
+//    @Bean
+//    public MemberRepository memberRepository(){
+////        return new MemoryMemberRepository();
+//
+//        // 순수 Jdbc
+////        return new JdbcMemberRepository(dataSource);
+//
+//        // Jdbc Template
+////        return new JdbcTemplateMemberRepository(dataSource);
+//
+//        //JPA
+//        return new JpaMemberRepository(em);
+//    }
 }
