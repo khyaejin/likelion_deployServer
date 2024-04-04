66278c7428a35421e835b817cc249800f241088d
khyaejin <olivia019182@naver.com>
2024-04-04 12:59:34 +0900

    Section 2-3 : API

--- hyejin/hw1/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hyejin/hw1/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,9 +1,13 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.processing.Generated;
 
 @Controller
 public class HelloController {
@@ -17,9 +21,43 @@ public class HelloController {
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello-template";
+        return "hello-template"; //html을 조작해서 화면에 내는 방식
     }
 
+    @GetMapping("hello-string")
+    @ResponseBody //html body에 return부 직접 들어감
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name; //hello spring 문자열만 띡 하고 화면에 내는 방식
+    }
+
+    /**
+     @ResponseBody 를 사용
+     - HTTP의 BODY에 문자 내용을 직접 반환
+     - viewResolver 대신에 HttpMessageConverter가 동작
+     - 기본 문자처리 : StringHttpMessageConverter가
+     - 기본 객체처리 : MappingJackson2HttpMessageConverter
+     - byte 처리 등등 기타 여러 HttpMessageConverter가 기본으로 등록되어 있음
+     **/
+
+    @GetMapping("hello-api")
+    @ResponseBody //객체로 반환하면 Json으로 들어가도록 기본적으로 정해져 있음
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
 
 // MVC: Model, View, Controller. 분리시킨 것
