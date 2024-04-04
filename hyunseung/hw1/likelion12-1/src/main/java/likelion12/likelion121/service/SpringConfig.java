f3270fc8e5d4444bfa188ffe8958f60182cd3a78
이현승 <77336664+gesal03@users.noreply.github.com>
2024-04-04 22:19:09 +0900

    Section 6-2: 순수 JDBC

--- hyunseung/hw1/likelion12-1/src/main/java/likelion12/likelion121/service/SpringConfig.java
+++ hyunseung/hw1/likelion12-1/src/main/java/likelion12/likelion121/service/SpringConfig.java
@@ -1,15 +1,24 @@
 package likelion12.likelion121.service;
 
+import likelion12.likelion121.repository.JdbcMemberRepository;
 import likelion12.likelion121.repository.MemberRepository;
 import likelion12.likelion121.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
     @Bean
     public MemberService memberService(){
