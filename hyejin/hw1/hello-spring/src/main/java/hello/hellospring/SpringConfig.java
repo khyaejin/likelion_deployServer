90804e533ae8316a33311a562fd4ffafebf9e157
khyaejin <olivia019182@naver.com>
2024-04-04 21:10:20 +0900

    Section 6-4 : 스프링 JdbcTemplate

--- hyejin/hw1/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hyejin/hw1/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -26,8 +27,10 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+
     }
 
     /**
