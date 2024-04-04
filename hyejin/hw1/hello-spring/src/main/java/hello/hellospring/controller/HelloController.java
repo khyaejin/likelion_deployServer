d7c8e2a6be1dc509e20b86ebaa0e5c29a1b2a5d5
khyaejin <olivia019182@naver.com>
2024-04-04 12:30:49 +0900

    Section 2-2 : MVC와 템플릿 엔진

--- hyejin/hw1/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hyejin/hw1/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,6 +11,18 @@ public class HelloController {
     @GetMapping("hello")//url에 매칭 -> controller에 있는 것들이 실행됨
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
-        return "hello";//resources:templates/hello.html을 열려라
+        return "hello";//resources:templates/hello.html을 열어라
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
+
+// MVC: Model, View, Controller. 분리시킨 것
+// View : 화면에 관련된 일
+// Controller : 비지니스 로직, 서버 뒷단에 관련 것 처리 후
+// Model : 모델에 담아 화면으로 넘겨주기
