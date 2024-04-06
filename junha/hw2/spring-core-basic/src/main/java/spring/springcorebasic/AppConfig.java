7a23f76caa6284aa1a17ec9329fdb8837d784dfe
junhaa <2171326@hansung.ac.kr>
2024-04-07 00:32:35 +0900

    :recycle: REFACTOR. Spring Bean Container

--- junha/hw2/spring-core-basic/src/main/java/spring/springcorebasic/AppConfig.java
+++ junha/hw2/spring-core-basic/src/main/java/spring/springcorebasic/AppConfig.java
@@ -1,5 +1,8 @@
 package spring.springcorebasic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import spring.springcorebasic.discount.DiscountPolicy;
 import spring.springcorebasic.discount.FixDiscountPolicy;
 import spring.springcorebasic.member.MemberRepository;
@@ -9,20 +12,25 @@ import spring.springcorebasic.member.MemoryMemberRepository;
 import spring.springcorebasic.order.OrderService;
 import spring.springcorebasic.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+
+	@Bean
 	public MemberService memberService(){
 		return new MemberServiceImpl(memberRepository());
 	}
 
+	@Bean
 	public OrderService orderService(){
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
-
+	@Bean
 	public MemberRepository memberRepository(){
 		return new MemoryMemberRepository();
 	}
 
+	@Bean
 	public DiscountPolicy discountPolicy(){
 		return new FixDiscountPolicy();
 	}
