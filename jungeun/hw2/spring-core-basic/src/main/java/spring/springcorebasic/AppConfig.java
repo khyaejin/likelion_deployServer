c432e1edb5a9532dfa8fd96b8f4a6c291bb02bbf
jungeun123 <147065885+jungeun123@users.noreply.github.com>
2024-04-07 14:01:09 +0900

    2-2

---
+++ jungeun/hw2/spring-core-basic/src/main/java/spring/springcorebasic/AppConfig.java
@@ -0,0 +1,29 @@
+package spring.springcorebasic;
+
+import spring.springcorebasic.discount.DiscountPolicy;
+import spring.springcorebasic.discount.FixDiscountPolicy;
+import spring.springcorebasic.discount.RateDiscountPolicy;
+import spring.springcorebasic.member.MemberRepository;
+import spring.springcorebasic.member.MemberService;
+import spring.springcorebasic.member.MemberServiceImpl;
+import spring.springcorebasic.member.MemoryMemberRepository;
+import spring.springcorebasic.order.OrderService;
+import spring.springcorebasic.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+}
