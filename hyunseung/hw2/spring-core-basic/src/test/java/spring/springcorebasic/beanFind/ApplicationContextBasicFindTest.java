e2ccd58abdb8463e95f94bd8a9f246f4b0a553e0
이현승 <77336664+gesal03@users.noreply.github.com>
2024-04-08 20:27:36 +0900

    TEST : Spring Bean 조회 - 기본(ByName)

---
+++ hyunseung/hw2/spring-core-basic/src/test/java/spring/springcorebasic/beanFind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,22 @@
+package spring.springcorebasic.beanFind;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.springcorebasic.AppConfig;
+import spring.springcorebasic.member.MemberService;
+import spring.springcorebasic.member.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("Spring Bean 조회 - ByName")
+    public void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class); // assetj
+    }
+}
