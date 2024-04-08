704187383e3b21c736f0d44c6d84e042a3e0cb6e
이현승 <77336664+gesal03@users.noreply.github.com>
2024-04-08 21:31:41 +0900

    TEST : SpringContainer와 SingleTon

--- hyunseung/hw2/spring-core-basic/src/test/java/spring/springcorebasic/singleTon/SingleTonTest.java
+++ hyunseung/hw2/spring-core-basic/src/test/java/spring/springcorebasic/singleTon/SingleTonTest.java
@@ -3,6 +3,10 @@ package spring.springcorebasic.singleTon;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.springcorebasic.AppConfig;
+import spring.springcorebasic.member.MemberService;
 
 public class SingleTonTest {
     @Test
@@ -16,4 +20,16 @@ public class SingleTonTest {
 
         Assertions.assertThat(singleTonService1).isSameAs(singleTonService2);
     }
+
+    @Test
+    @DisplayName("SpringContainer와 SingleTon")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+    }
+
 }
