b03c2f14f8746cc239c706e1ad9cbba67da769c1
khyaejin <olivia019182@naver.com>
2024-04-11 16:16:01 +0900

    FEAT : discountPercent

--- hyejin/hw2/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
+++ hyejin/hw2/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package spring.springcorebasic.order;
 
 import spring.springcorebasic.discount.DiscountPolicy;
 import spring.springcorebasic.discount.FixDiscountPolicy;
+import spring.springcorebasic.discount.RateDiscountPolicy;
 import spring.springcorebasic.member.Member;
 import spring.springcorebasic.member.MemberRepository;
 import spring.springcorebasic.member.MemoryMemberRepository;
@@ -10,7 +11,7 @@ public class OrderServiceImpl implements OrderService{
 
     // 회원의 등급을 확인하기 위함 => 할인정책 적용을 위해
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId); // 사용자의 등급을 통해 할인정책을 달리 하기 위함
