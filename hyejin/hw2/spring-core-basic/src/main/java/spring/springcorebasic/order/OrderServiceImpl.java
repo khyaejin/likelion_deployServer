b9b72a9b59d3f010612e43ca548161f49574affc
khyaejin <olivia019182@naver.com>
2024-04-11 17:31:43 +0900

    Refactor : DIP를 위반하지 않도록 생성자를 사용하여 의존관계 주입하도록 변경

--- hyejin/hw2/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
+++ hyejin/hw2/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
@@ -10,8 +10,19 @@ import spring.springcorebasic.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     // 회원의 등급을 확인하기 위함 => 할인정책 적용을 위해
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    private final MemberRepository memberRepository ;
+    private DiscountPolicy discountPolicy;
+    // DIP 위반 -> 구체에 의존하지 않고 추상에만 의존하도록 변경
+    // 그렇다면 discountPolicy의 초기화는 어떻게? -> 누군가 DiscountPolicy의 구현 객체를 생성해줘야 함
+    // 단일 책임의 원칙.
+
+    // 생성자를 사용하여 의존관계 주입(생성자 주입)
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId); // 사용자의 등급을 통해 할인정책을 달리 하기 위함
