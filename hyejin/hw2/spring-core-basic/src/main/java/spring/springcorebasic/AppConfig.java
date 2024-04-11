ad63a1806612e22eda9266c914fd156e87186346
khyaejin <olivia019182@naver.com>
2024-04-11 17:44:32 +0900

    REFACTOR : 기존 코드들을 AppConfig 사용하도록 변경

---
+++ hyejin/hw2/spring-core-basic/src/main/java/spring/springcorebasic/AppConfig.java
@@ -0,0 +1,28 @@
+package spring.springcorebasic;
+
+import spring.springcorebasic.discount.DiscountPolicy;
+import spring.springcorebasic.discount.FixDiscountPolicy;
+import spring.springcorebasic.member.MemberRepository;
+import spring.springcorebasic.member.MemberService;
+import spring.springcorebasic.member.MemberServiceImpl;
+import spring.springcorebasic.member.MemoryMemberRepository;
+import spring.springcorebasic.order.OrderService;
+import spring.springcorebasic.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService(){
+        return new MemberServiceImpl( memberRepository());
+    }
+
+    public OrderService orderSercice(){
+        return new OrderServiceImpl( memberRepository(), new FixDiscountPolicy());
+    }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+}
