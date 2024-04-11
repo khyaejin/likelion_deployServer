256713a59bb239514da9bc8c680409a4429967bb
khyaejin <olivia019182@naver.com>
2024-04-11 21:45:36 +0900

    TEST : 빈 이름으로 조회

---
+++ hyejin/hw2/spring-core-basic/src/main/java/spring/springcorebasic/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,25 @@
+package spring.springcorebasic.beanfind;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.springcorebasic.member.MemberService;
+import spring.springcorebasic.member.MemberServiceImpl;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회");
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+
+    }
+
+
+}
