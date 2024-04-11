249e67a7d581a6cf09ee8da87fd37300f06aff9d
khyaejin <olivia019182@naver.com>
2024-04-11 21:35:36 +0900

    TEST : 어플리케이션 빈 출력하기

--- hyejin/hw2/spring-core-basic/src/main/java/spring/springcorebasic/beanfind/ApplicationContextInfoTest.java
+++ hyejin/hw2/spring-core-basic/src/main/java/spring/springcorebasic/beanfind/ApplicationContextInfoTest.java
@@ -2,15 +2,16 @@ package spring.springcorebasic.beanfind;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.springcorebasic.AppConfig;
 
 public class ApplicationContextInfoTest {
-    ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
-    @DisplayName("모든 Bean 출력하기")
+    @DisplayName("모든 빈 출력하기")
     public void findAllBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
@@ -19,4 +20,16 @@ public class ApplicationContextInfoTest {
         }
     }
 
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    public void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + ", object = " + bean);
+            }
+        }
+    }
 }
