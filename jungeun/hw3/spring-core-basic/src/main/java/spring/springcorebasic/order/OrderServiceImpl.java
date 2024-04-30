164d06616814c5be5d8daec530bf1c88551d460d
jungeun123 <147065885+jungeun123@users.noreply.github.com>
2024-04-30 21:38:01 +0900

    3-2. @RequiredArgsConstructor 추가

--- jungeun/hw3/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
+++ jungeun/hw3/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
@@ -1,22 +1,18 @@
 package spring.springcorebasic.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import spring.springcorebasic.discount.DiscountPolicy;
 import spring.springcorebasic.member.Member;
 import spring.springcorebasic.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     // 회원의 등급을 확인하기 위함 => 할인정책 적용을 위해
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
