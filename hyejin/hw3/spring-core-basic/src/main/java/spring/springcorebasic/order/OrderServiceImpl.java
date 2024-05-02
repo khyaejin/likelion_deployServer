db444ccbb646fe0c70a4512531fcea367b003236
khyaejin <olivia019182@naver.com>
2024-05-02 18:23:25 +0900

    FEAT : OrderServicempl에 @RequiredArgsConstructor 추가

--- hyejin/hw3/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
+++ hyejin/hw3/spring-core-basic/src/main/java/spring/springcorebasic/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package spring.springcorebasic.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import spring.springcorebasic.discount.DiscountPolicy;
@@ -10,22 +11,23 @@ import spring.springcorebasic.member.MemberRepository;
 import spring.springcorebasic.member.MemoryMemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     // 회원의 등급을 확인하기 위함 => 할인정책 적용을 위해
 
     private final MemberRepository memberRepository ;
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy;
     // DIP 위반 -> 구체에 의존하지 않고 추상에만 의존하도록 변경
     // 그렇다면 discountPolicy의 초기화는 어떻게? -> 누군가 DiscountPolicy의 구현 객체를 생성해줘야 함
     // 단일 책임의 원칙.
 
     // 생성자를 사용하여 의존관계 주입(생성자 주입)
     // @Autowired : 생성자가 하나만 있으면 @Autowired 지울 수 있음
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
